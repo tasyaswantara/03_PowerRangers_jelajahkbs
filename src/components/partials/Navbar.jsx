216917e7f193d55684ad7d5bd3d786e264a43280
tasyaswantara <tasyaswantara@gmail.com>
2024-04-27 07:37:59 +0700

    style: fix logo

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -1,7 +1,8 @@
+import Logo from "../../assets/icon/Logo_Zoo.svg"
 const Navbar = () => {
   return (
     <div className=" w-full h-[8vh] flex justify-center items-center gap-6 bg-secondary-yellow">
-            <div className=" bg-logo w-20 h-10 bg-cover"></div>
+            <img src={Logo} className=" bg-logo w-20 h-10 bg-cover"/>
             <h1 className=" text-primary-green font-extrabold">Kebun Binatang Surabaya</h1>
         </div>
     );
