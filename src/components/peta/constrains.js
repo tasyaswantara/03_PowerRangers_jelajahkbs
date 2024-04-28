bf6e93a6fd7498960281b4a66a4feaf911547dca
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-04-28 07:17:01 +0700

    03_PowerRangers_jelajahkbs\src\components\peta\constrains.js

---
+++ src/components/peta/constrains.js
@@ -0,0 +1,32 @@
+const status = [
+    {
+        status: "Tidak dikhawatirkan",
+        color: "7CBF69",
+        left: "KEKURANGAN DATA",
+        right: "HAMPIR TERANCAM"
+    },
+    {
+        status: "Rentan",
+        color: "DEA000",
+        left: "HAMPIR TERANCAM",
+        right: "TERANCAM"
+    },
+    {
+        status: "Hampir terancam",
+        color: "FF0606",
+        left: "TIDAK DIKHAWATIRKAN",
+        right: "RENTAN"
+    },
+    {
+        status: "Terancam punah",
+        color: "FA4B00",
+        left: "RENTAN",
+        right: "TERANCAM KRITIS"
+    },
+    {
+        status: "Sangat terancam punah",
+        color: "FF0606",
+        left: "TERANCAM",
+        right: "TIDAK ADA"
+    }
+]
